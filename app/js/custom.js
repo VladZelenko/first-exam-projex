0e7a4ae15c557b194f41531353156c45893c502a
VladZelenko <Vlad6555@ukr.net>
2017-04-20 23:07:31 +0300

    add hero section, slick-carousel + style

--- app/js/custom.js
+++ app/js/custom.js
@@ -7,3 +7,10 @@ function closeNav(){
 	document.getElementById('nav').style.width = "0%";
 	document.getElementById('open-btn-nav').style.opacity = "1";
 }
+
+$('.hero-slider').slick({
+  infinite: true,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
