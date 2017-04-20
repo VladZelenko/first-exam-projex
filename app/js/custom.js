bbeaac85fb916106cd16b171ceee5b6dd76123b8
VladZelenko <Vlad6555@ukr.net>
2017-04-20 22:49:13 +0300

    add header + nav-menu + nobile full sreen menu and style

---
+++ app/js/custom.js
@@ -0,0 +1,9 @@
+//mobile menu
+function openNav(){
+	document.getElementById('nav').style.width = "100%";
+	document.getElementById('open-btn-nav').style.opacity = "0";
+}
+function closeNav(){
+	document.getElementById('nav').style.width = "0%";
+	document.getElementById('open-btn-nav').style.opacity = "1";
+}
